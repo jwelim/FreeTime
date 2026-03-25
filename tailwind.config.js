1ed9be14810c1153a35f757c41b863bb42e35cc6
jwelim <140692941+jwelim@users.noreply.github.com>
2026-03-25 10:12:18 +0900

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./app.jsx", "./freetime/*.jsx", "./freetime/*.html"],
-  theme: { extend: {} },
-  plugins: [],
-};
